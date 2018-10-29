925611a215615b5fa8128d25693b39491e820dd0
fireflyoo <lufeng369@gmail.com>
2018-10-29 15:16:07 +0800

    Add files via upload

--- gen-5rc4.sh
+++ gen-5rc4.sh
@@ -1,29 +1,30 @@
 #!/bin/sh
 
 URL="http://malibu.tuxfamily.net/slitaz"
-ISO_URL="${URL}/iso/rolling/slitaz-rolling-core64.iso"
+ISO_URL="${URL}/iso/rolling/slitaz-rolling-core.iso"
 ISO=$(basename $ISO_URL)
 ISO_PATH="/home/slitaz/5.0/distro"
 PKG_MIRROR="${URL}/packages/5.0/"
-
+IME_URL="https://github.com/fireflyoo/slitaz-cn/raw/master/yong-2.5.0.tazpkg"
+IME=$(basename $IME_URL)
 tazlito clean-distro
 mkdir -p $ISO_PATH
-cd $ISO_PATH
-if [ ! -e ../$ISO ]; then
-    busybox wget -P .. $ISO_URL;
-fi
-if [ -e ../$ISO ]; then
-    tazlito extract-distro ../$ISO;
-fi
-cat << EOF | chroot rootfs
+
+[ ! -e $ISO ]&& wget -P . $ISO_URL
+[ -e $ISO ] && tazlito extract-distro $ISO
+echo "$URL/packages/5.0/" > $ISO_PATH/rootfs/var/lib/tazpkg/mirror
+[ ! -e $IME ] && wget -P . $IME_URL
+tazpkg install $IME --root=$ISO_PATH/rootfs --nodeps
+tazpkg get wqy-microhei
+tazpkg install wqy-microhei*.tazpkg --root=$ISO_PATH/rootfs --nodeps --local 
+tazpkg get locale-zh_CN
+tazpkg install locale-zh_CN*.tazpkg --root=$ISO_PATH/rootfs --nodeps --local
+cat << EOF | chroot $ISO_PATH/rootfs
 echo hwclock -t >> /etc/init.d/local.sh
-#echo ntpd -nq -p cn.ntp.org.cn >> /etc/init.d/local.sh
+echo "#ntpd -nq -p cn.ntp.org.cn" >> /etc/init.d/local.sh
 sed -i 's/ntpd//' /etc/rcS.conf
-echo http://malibu.tuxfamily.net/slitaz/packages/5.0/ > /var/lib/tazpkg/mirror
 rm /usr/share/locale/* -rf
-gtk-query-immodules-2.0 --update-cache
-udhcpc -i eth0
-tazpkg recharge
+#udhcpc -i eth0
 yes | tazpkg remove locale-es
 yes | tazpkg remove locale-de
 yes | tazpkg remove locale-fr
@@ -31,13 +32,19 @@ yes | tazpkg remove locale-it
 yes | tazpkg remove locale-pt
 yes | tazpkg remove locale-pt_BR
 yes | tazpkg remove locale-ru
-tazpkg get-install locale-zh_CN
 tazlocale zh_CN
-tazpkg get-install fcitx
 tazpkg clean-cache
-sed -i  '4i\export XMODIFIERS="@im=fcitx"' /etc/skel/.xinitrc
-sed -i '5i\export GTK_IM_MODULE=xim' /etc/skel/.xinitrc
-sed -i '6i\fcitx &' /etc/skel/.xinitrc
+cd /var/lib/tazpkg
+rm packages.*
+rm ID*
+rm files*
+rm *.txt
+rm extra.list
+gtk-query-immodules-2.0 --update-cache
+#gtk-query-immodules-3.0 --update-cache
+sed -i  '4i\export XMODIFIERS="@im=yong"' /etc/skel/.xinitrc
+sed -i '5i\export GTK_IM_MODULE=yong' /etc/skel/.xinitrc
+sed -i '6i\yong &' /etc/skel/.xinitrc
 sed -i 's/Monospace/DejaVu Sans Mono/' /etc/skel/.Xdefaults
 EOF
 
